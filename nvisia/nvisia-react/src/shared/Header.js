13641c1e04b6fb16267838fb6e33a13e34f896bd
withoutwax <rlagmlckd@gmail.com>
2018-04-24 16:42:10 -0500

    Implemented Header-Navbar

--- nvisia/nvisia-react/src/shared/Header.js
+++ nvisia/nvisia-react/src/shared/Header.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import './Header.css';
+import NVISIA from '../assets/symbols/logo01.png'
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <navbar className="header-nav">
+        <img className="nvisia-logo" src={NVISIA} alt="NVISIA-logo"/>
+        <a>ABOUT</a>
+        <a>WORK</a>
+        <a>SERVICES</a>
+        <a>BLOG</a>
+        <a>EVENTS</a>
+        <a>CAREERS</a>
+        <a className="margin-right-50">CONTACT US</a>
+      </navbar>
     );
   }
 }
