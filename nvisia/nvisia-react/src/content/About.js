ceaee7355f17b02c4879e6c774d9f891a7a0a184
withoutwax <rlagmlckd@gmail.com>
2018-04-26 14:49:14 -0500

    About-Section-03

--- nvisia/nvisia-react/src/content/About.js
+++ nvisia/nvisia-react/src/content/About.js
@@ -8,6 +8,8 @@ import connectBlack from '../assets/connect-black.png';
 
 import headerBanner02 from '../assets/header-banner/Header-Banner-02.jpg';
 import aboutBanner01 from '../assets/about/about-banner-01.png';
+import connectBig from '../assets/about/about-connect-big.png';
+
 class About extends Component {
   render() {
     return (
@@ -39,7 +41,7 @@ class About extends Component {
           <img src={connectOrange} />
           <article className="about-section-02-banner-text padding-right-200">
             <h2>
-              About.
+              OUR MISSION.
             </h2>
             <br/>
             <p>
@@ -48,9 +50,44 @@ class About extends Component {
           </article>
         </section>
 
-        {/* Video */}
-        <section className="about-vimeo-01-container margin-bottom-100">
+        {/* About - Vimeo Video */}
+        <section className="about-vimeo-01-container padding-bottom-100">
           <ReactPlayer className="about-vimeo-01" url="https://vimeo.com/erichinesphotography/cityscapechicago" width="90%" height="700px" playing loop />
+        </section>
+
+        <section className="about-section-03-container padding-top-200 padding-bottom-200">
+          <img src={connectOrange} />
+          <article className="about-section-03-banner-text padding-right-200">
+            <h2>
+              OUR PURPOSE.
+            </h2>
+            <br/>
+            <p>
+              <strong>NVISIA</strong> was born at the nexus of three goals:
+            </p>
+            <div className="about-section-03-article margin-top-50">
+              <img className="about-section-03-article-img-01" src={connectBig} />
+              <div>
+                <h2>OUR PEOPLE</h2>
+                <p>
+                  People are the most important element to any build; we work side-by-side with our clients to form an agile, committed team that takes pride in building the solution.
+                </p>
+              </div>
+              <div>
+                <h2>FORERUNNER</h2>
+                <p>
+                  These are the foundation of our company, guiding us in all we do, and inspiring us to continuously grow and evolve.
+                </p>
+              </div>
+              <div>
+                <h2>COMPETITIVE EDGE</h2>
+                <p>
+                  These are the foundation of our company, guiding us in all we do, and inspiring us to continuously grow and evolve.
+                </p>
+              </div>
+            </div>
+
+          </article>
         </section>
       </div>
     );
