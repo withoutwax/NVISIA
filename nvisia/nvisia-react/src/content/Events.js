3e15c00e7c41bd5a2e5656bf4330534eb9465dae
withoutwax <rlagmlckd@gmail.com>
2018-05-08 12:20:26 -0500

    Implemented re-usable components for Events card layout

--- nvisia/nvisia-react/src/content/Events.js
+++ nvisia/nvisia-react/src/content/Events.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import Cards from './EventsCard';
+import './Events.css';
+
+import connectOrange from '../assets/connect-orange.png';
+import connectWhite from '../assets/connect-white.png';
+import connectBlack from '../assets/connect-black.png';
+import headerBanner04 from '../assets/header-banner/Header-Events.png';
+
+import logo02 from '../assets/symbols/logo02.png'; // NVISIA LOGO
 
 class Events extends Component {
   render() {
     return (
       <div>
-        Events
+        <section className="section-00-container">
+          <img src={headerBanner04} />
+          <article className="section-00-banner-text">
+            <h1>
+              EVENTS.
+            </h1>
+            <br/>
+            <p>
+              NVISIA is dedicated to fostering an environment where the<br/>
+              most talented engineers can do their best work.
+            </p>
+          </article>
+        </section>
+
+        {/* SECTION 01 */}
+        <section className="events-section-01-container padding-top-200 padding-bottom-200">
+          <article className="events-section-01-banner-title">
+            <img src={connectOrange} width="50px" alt="connect-white"/>
+            <h2>EVENTS</h2>
+            <h1>1</h1>
+          </article>
+          <article className="events-section-01-banner-text">
+            <Cards
+              title="MILWAUKEE DIGITAL PLATFORM CONFERENCE"
+              logo={logo02}
+              location="MILWAUKEE"
+              date="May | TBD | TBD"
+            />
+          </article>
+        </section>
+
       </div>
     );
   }
---
+++ nvisia/nvisia-react/src/content/EventsCard.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './EventsCard.css';
+
+// import logo02 from '../assets/symbols/logo02.png';
+
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.title,
+      logo: props.logo,
+      location: props.location,
+      date: props.date,
+    }
+  }
+
+  render() {
+    return (
+      <div className="events-card">
+        <h4 id="events-card-title">{this.state.title}</h4>
+        <img id="events-card-img" src={this.state.logo} />
+        <div id="events-card-date">
+          <p>{this.state.location}</p>
+          <p>{this.state.date}</p>
+        </div>
+        <button type="button" name="about-us" id="events-card-button" className="margin-top-60">
+          CLICK TO REGISTER
+        </button>
+      </div>
+
+    );
+  }
+}
+export default Cards;
